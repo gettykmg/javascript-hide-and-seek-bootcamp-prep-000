65b3be6aa5c7ff982cdbf1ed56b9bdeb69aedaa1
Keith Getty <keithgetty@yahoo.com>
2017-10-03 01:42:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild() {
+  let list = document.getElementById('grand-node').querySelectorAll('div');
+  return list[list.length-1];
+}
+
+function increaseRankBy(n) {
+   var ranks = document.querySelectorAll('ul.ranked-list li');
+   for (var i=0; i < ranks.length; i++) {
+     ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+   }
+ }
